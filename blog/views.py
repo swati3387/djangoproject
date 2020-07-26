ce44d72d9e4e6c0a21d01808c041f8a53ed81900
swati3387 <bajajswatiedu@gmail.com>
2020-07-26 20:51:56 +0530

    added details post template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -8,3 +8,9 @@ def post_list(request):
     posts= Post.objects.all()
     context={"posts": posts}
     return render(request,'blog/post_list.html',context)
+def post_detail(request, pk):
+    #return HttpResponse("<b>Here's the text of the Web page.</b>")
+    post= get_object_or_404(Post,pk=pk)
+    context={"post": post}
+    return render(request,'blog/post_detail.html',context)
+
