c9c147a69ddc6c2ac379d72ad67cb5cbe1c062cd
swati3387 <bajajswatiedu@gmail.com>
2020-07-26 19:34:04 +0530

    Modified Template to display post from database

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
     #return HttpResponse("<b>Here's the text of the Web page.</b>")
-    context={}
+    posts= Post.objects.all()
+    context={"posts": posts}
     return render(request,'blog/post_list.html',context)
