62e894633edee5e91ae9f9fb0ba148b58819766b
swati3387 <bajajswatiedu@gmail.com>
2020-07-26 19:03:44 +0530

    Added html page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    #return HttpResponse("<b>Here's the text of the Web page.</b>")
+    context={}
+    return render(request,'blog/post_list.html',context)
